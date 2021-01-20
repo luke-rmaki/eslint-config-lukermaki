7d9e9ab8e8d29999c99ec912add52460fa3073de
Luke Ruokaismaki <luke@rmaki.com.au>
2021-01-20 14:38:55 +1100

    feat: Turn off camel case

--- .eslintrc.js
+++ .eslintrc.js
@@ -50,6 +50,7 @@ module.exports = {
     'react/forbid-prop-types': 0,
     'react/no-unescaped-entities': 0,
     'import/prefer-default-export': 0,
+    camelcase: 0,
     import: 0,
     'react/jsx-filename-extension': [
       1,
